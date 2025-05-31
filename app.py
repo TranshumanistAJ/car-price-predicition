aa16b0bd1153f0bfdc766f7adf2fb152b23a00e5
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-05-31 17:53:09 +0000

    model train in 3_feature_engineering.ipynb and test works

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ import pandas as pd
 
 st.title('Car Price Prediction')
 
-st.markdown('### Load Dataset')
+st.markdown('### Load original Dataset')
 df = pd.read_csv('datasets/car_dataset.csv')
 
-st.dataframe(df)
+st.dataframe(df.head(5))
 
 
 st.divider()
 
 
-st.markdown('### information')
+st.markdown('### Clean Data')
+df = df.dropna().reset_index(drop = True)
+st.dataframe(df)
+
 
