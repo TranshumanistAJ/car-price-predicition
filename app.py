104ac927cfd08239db9930725e70c6e43d5059b9
TranshumanistAJ <AJDeutschland103@hotmail.com>
2025-05-31 14:53:41 +0000

    load and test dataset and start streamlit app with loaded data

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import streamlit as st 
+import pandas as pd
 
 
 st.title('Car Price Prediction')
+
+st.markdown('### Load Dataset')
+df = pd.read_csv('datasets/car_dataset.csv')
+
+st.dataframe(df)
+
+
+st.divider()
+
+
+st.markdown('### information')
 
